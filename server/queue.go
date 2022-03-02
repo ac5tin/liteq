f6b8152ee150d7fea655c3bceda03235f817bf2a
austin <austinchang4@gmail.com>
2022-03-02 14:12:38 +0800

    stream all existing tasks and end

--- server/queue.go
+++ server/queue.go
@@ -10,29 +10,30 @@ import (
 
 // QueueService
 func (*server) GetTasks(empty *emptypb.Empty, stream proto.LiteQ_GetTasksServer) error {
-	for {
-		/* Receive stream message
-		 taskType, err := stream.Recv()
-		log.Println("GetTasks:", taskType)
-		// error handling
-		{
-			if err == io.EOF {
-				log.Println("EOF")
-				return nil
-			}
-			if err != nil {
-				return err
-			}
-		}
-		*/
-		for _, t := range *queue.Q.Tasks {
-			stream.Send(&proto.Task{
-				Id:        t.ID,
-				Data:      t.Data,
-				CreatedAt: timestamppb.New(t.CreationDate),
-				Status:    *proto.Task_TaskStatus(t.Status).Enum(),
-			})
-		}
 
+	/* Receive stream message
+	 taskType, err := stream.Recv()
+	log.Println("GetTasks:", taskType)
+	// error handling
+	{
+		if err == io.EOF {
+			log.Println("EOF")
+			return nil
+		}
+		if err != nil {
+			return err
+		}
 	}
+	*/
+
+	for _, t := range *queue.Q.Tasks {
+		stream.Send(&proto.Task{
+			Id:        t.ID,
+			Data:      t.Data,
+			CreatedAt: timestamppb.New(t.CreationDate),
+			Status:    *proto.Task_TaskStatus(t.Status).Enum(),
+		})
+	}
+
+	return nil
 }
